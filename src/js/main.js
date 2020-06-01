a7cf8d93969ba35fae368561f5292699eec43eab
Bas <bdegreeuw@ziggo.nl>
2020-06-01 14:15:09 +0200

    Built a node application that allows uploading to the API

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
-import '../scss/main.scss';
-console.log("hello world!");
+import '../scss/main.scss'
+import CanvasNest from 'canvas-nest.js';
+
+init();
+
+function init() {
+    const background = initNests(document.querySelector('.background'));
+}
+
+function initNests(element) {
+    const config = {
+        color: '255,255,255',
+        pointColor: '125, 125, 125',
+        count: 120,
+    };
+    return new CanvasNest(element, config);
+}
