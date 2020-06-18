8d1ea87362b23117013f2878ec4b450fef9102fc
Bas <bdegreeuw@ziggo.nl>
2020-06-18 12:44:05 +0200

    Update sw.js
    cahing HTML

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,5 +1,19 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'document',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'documents',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    }),
+);
+
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst({
