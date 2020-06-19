ec3055ad854b846b3b9c5ba3f76842b623a82324
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-06-19 11:57:39 +0200

    please import

--- src/js/options.js
+++ src/js/options.js
@@ -1,69 +1,64 @@
-if(document.getElementById("application")){
-    var application = document.getElementById('application')
-}
+ export function menuOptions(){
+    const application = document.getElementById("application") ? document.getElementById("application") : false
 
-if (document.getElementById("options")){
-    let optionsButton = document.getElementById("options")
-    optionsButton.onclick = function(){
-        let optionsMenu = document.getElementById("optionsMenu")
-        optionsMenu.classList.toggle("options")
+    
+    if (document.getElementById("options")){
+        let optionsButton = document.getElementById("options")
+        optionsButton.onclick = function(){
+            let optionsMenu = document.getElementById("optionsMenu")
+            optionsMenu.classList.toggle("options")
+        };
     };
-};
+    
+    if (document.getElementById('confirmation')){
+        let confirmationButton = document.getElementById('confirmation')
+        confirmationButton.addEventListener("click", function(event){
+            event.preventDefault()
+            optionsMenu.classList.toggle("options")
+        })
+    }
+    
+    const accessibility = document.getElementById('accessibility')
+    console.log(accessibility.colorblindness.value)
+    
+    const fontcontrol = document.getElementById('graphs')
+    console.log(fontcontrol.fontsize.value)
+    
+    fontcontrol.onchange = function(event){
+        let fontSize = event.target.value
+        if (fontSize == 12){
+            console.log('smaller')
+            application.style.fontSize = '14px'
+    
+        } else if (fontSize == 14){
+            console.log('medium')
+            application.style.fontSize ='16px'
+    
+        } else if (fontSize == 16){
+            console.log('bigger')
+            application.style.fontSize = '18px'
+    
+        }
+    }
+    
+    accessibility.onchange = function(event){
+        let targetValue = event.target.value
+        if (targetValue == 1){
+    
+            application.style.fill = 'purple'
+            application.style.color = 'purple'
+        } else if(targetValue == 2){
+        
+            application.style.fill = 'red'
+            application.style.color = 'red'
+        } else if(targetValue == 3){
+            
+            application.style.fill = 'purple'
+            application.style.color = 'purple'
+        } else if (targetValue == 0){
 
-if (document.getElementById('confirmation')){
-    let confirmationButton = document.getElementById('confirmation')
-    confirmationButton.addEventListener("click", function(event){
-        event.preventDefault()
-        optionsMenu.classList.toggle("options")
-    })
-}
-
-var accessibility = document.getElementById('accessibility')
-console.log(accessibility.colorblindness.value)
-
-var fontcontrol = document.getElementById('graphs')
-console.log(fontcontrol.fontsize.value)
-
-fontcontrol.onchange = function(event){
-    let fontSize = event.target.value
-    if (fontSize == 12){
-        console.log('smaller')
-        application.style.fontSize = '14px'
-
-    } else if (fontSize == 14){
-        console.log('medium')
-        application.style.fontSize ='16px'
-
-    } else if (fontSize == 16){
-        console.log('bigger')
-        application.style.fontSize = '18px'
-
+            application.style.fill = 'black'
+            application.style.color = 'black'
+        }
     }
 }
-
-accessibility.onchange = function(event){
-    let targetValue = event.target.value
-    if (targetValue == 1){
-        console.log("Green")
-        application.style.fill = 'purple'
-        application.style.color = 'purple'
-    } else if(targetValue == 2){
-        console.log("Red")
-        application.style.fill = 'red'
-        application.style.color = 'red'
-    } else if(targetValue == 3){
-        console.log("blue")
-        application.style.fill = 'purple'
-        application.style.color = 'purple'
-    } else if (targetValue == 0){
-        console.log('no blind')
-        application.style.fill = 'black'
-        application.style.color = 'black'
-    } else {
-        console.log('no value read')
-    }
-}
-
-
-// let tree = document.getElementsByClassName('tree').item(0)
-// console.log(tree)
