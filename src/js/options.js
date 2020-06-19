1d4809d4214cb945031048cff7f66d25809b0df5
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-06-19 10:57:21 +0200

    change colors and font size

--- src/js/options.js
+++ src/js/options.js
@@ -45,15 +45,25 @@ accessibility.onchange = function(event){
     let targetValue = event.target.value
     if (targetValue == 1){
         console.log("Green")
+        application.style.fill = 'purple'
+        application.style.color = 'purple'
     } else if(targetValue == 2){
         console.log("Red")
+        application.style.fill = 'red'
+        application.style.color = 'red'
     } else if(targetValue == 3){
         console.log("blue")
+        application.style.fill = 'purple'
+        application.style.color = 'purple'
     } else if (targetValue == 0){
         console.log('no blind')
+        application.style.fill = 'black'
+        application.style.color = 'black'
     } else {
         console.log('no value read')
     }
 }
 
 
+// let tree = document.getElementsByClassName('tree').item(0)
+// console.log(tree)
