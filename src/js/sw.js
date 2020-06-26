c7fd90ea4a681a675856ceff13170663beb133fa
Bas <bdegreeuw@ziggo.nl>
2020-06-26 11:55:57 +0200

    Added SVG to build route and register styling

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,19 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-// workbox.routing.registerRoute(
-//     ({request}) => request.destination === 'document',
-//     new workbox.strategies.StaleWhileRevalidate({
-//         cacheName: 'documents',
-//         plugins: [
-//             new workbox.expiration.ExpirationPlugin({
-//                 maxEntries: 60,
-//                 maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//             }),
-//         ],
-//     }),
-// );
-
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst({
@@ -30,15 +17,13 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     ({request}) => new URL(request.url).pathname === '/data/getentities/old',
     async ({url, event}) => {
-            return caches.open('entities').then((cache) => {
-                event.request.url = event.request.url.replace('old', 'new');
-                return cache.match('/data/getentities/new').then((response) => {
-                    return response || fetch(event.request).then(function (response) {
-                        cache.put('/data/getentities/new', response.clone());
-                        return response;
-                    })
-                })
-            })
+        const cache = await caches.open('entities');
+        event.request.url = event.request.url.replace('old', 'new');
+        const response = await cache.match('/data/getentities/new')
+        return response || fetch(event.request).then(function (response) {
+            cache.put('/data/getentities/new', response.clone());
+            return response;
+        })
     }
 )
 
